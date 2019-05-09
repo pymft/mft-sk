77e96dee36e5c882de2c7ccfc21acbc58622f255
iamvee <naeini.v@gmail.com>
2019-05-09 09:40:14 +0430

    decorators

--- S07/decorators/what_is_decorator.py
+++ S07/decorators/what_is_decorator.py
@@ -1,18 +1,18 @@
 def fun_weird(f):
     def out(x):
-
-        res = f(x)
-
+        res = "\033[31;1m" + str(f(x)) + "\033[0m"
         return res
-    return out
 
+    return out
 
 
 def echo(s):
     return s
 
+
+echo_new = fun_weird(echo)
+res = echo_new("hello")
+print(res)
+
 new_len = fun_weird(len)
-new_max = fun_weird(max)
-val = new_len([1, 2, 3])
-val = new_max([1, 2, 3])
-print(val)
+print(new_len([1, 2, 3, 43]))
