f903cf7d5d5ee5599e371532c039f85e961ddd45
iamvee <naeini.v@gmail.com>
2019-05-09 09:15:39 +0430

    decorator - step2

--- S07/decorators/what_is_decorator.py
+++ S07/decorators/what_is_decorator.py
@@ -1,14 +1,18 @@
 def fun_weird(f):
-    # out = len
-    out = lambda x: f(x)
+    def out(x):
+
+        res = f(x)
+
+        return res
     return out
 
 
-new_len = fun_weird(len)
-def new_len(x):
-    return len(x)
-new_max = fun_weird(max)
-new_min = fun_weird(min)
 
+def echo(s):
+    return s
+
+new_len = fun_weird(len)
+new_max = fun_weird(max)
 val = new_len([1, 2, 3])
+val = new_max([1, 2, 3])
 print(val)
